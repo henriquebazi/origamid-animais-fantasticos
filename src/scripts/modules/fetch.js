2dcf58b08c5b1d8b195012a2692d50f08fce5093
henriquebazi <henriquebazi96@gmail.com>
2021-08-23 08:31:26 -0300

    refatored fetchAnimals

--- src/scripts/modules/fetch.js
+++ src/scripts/modules/fetch.js
@@ -1,36 +1,41 @@
 import NumbersAnimation from './animations.js'
 
-async function initFetchAnimais(url) {
-  const numbersGrid = document.querySelector('.numbers-grid')
-  
-  try {
-    const animalsResponse = await fetch(url)
-    const animalsJSON = await animalsResponse.json()
+export default function fetchAnimal(url, target) {
+  function createAnimalDiv(animal) {
+    const div = document.createElement('div')  
+    
+    div.classList.add('animal-number')
+    div.innerHTML = `<h3>${animal.specie}</h3><span data-number>${animal.total}</span>`
 
-    function createAnimal(animal) {
-      const div = document.createElement('div')  
-      
-      div.classList.add('animal-number')
-      div.innerHTML = `<h3>${animal.specie}</h3><span data-number>${animal.total}</span>`
+    return div
+  }
 
-      return div
-    }
+  const numbersGrid = document.querySelector(target)
+  function setAnimals(animal) {
+    const divAnimal = createAnimalDiv(animal)
+        
+    numbersGrid.appendChild(divAnimal)
+  }
 
-    animalsJSON.forEach(animal => {
-      const divAnimal = createAnimal(animal)
-      
-      numbersGrid.appendChild(divAnimal)
-    })
-
+  function animalAnimationNumbers() {
     const numbersAnimation = new NumbersAnimation('[data-number]', '.numbers', 'active')
     numbersAnimation.init()
-
-  } catch(error) {
-    console.log(error)
   }
-}
 
-initFetchAnimais('../../public/animaisapi.json')
+  async function createAnimal() {
+    try {
+      const animalsResponse = await fetch(url)
+      const animalsJSON = await animalsResponse.json()
+      animalsJSON.forEach(animal => setAnimals(animal))
+  
+      animalAnimationNumbers()  
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
+  return createAnimal()
+}
 
 async function initFetchBitcoin(url) {
   const btcPrice = document.querySelector('.btc-price')
