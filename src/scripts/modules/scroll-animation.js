31c317ca634197e35bf103315ba0df54163fa197
henriquebazi <henriquebazi96@gmail.com>
2021-08-24 19:32:44 -0300

    refatored scroll animation to class

--- src/scripts/modules/scroll-animation.js
+++ src/scripts/modules/scroll-animation.js
@@ -3,26 +3,41 @@ export default class ScrollAnimation {
     this.sections = document.querySelectorAll(sections)
     this.windowHalf = window.innerHeight / 2
 
-    this.animationScroll = this.animationScroll.bind(this)
+    this.checkDistance = this.checkDistance.bind(this)
   }
   
-  animationScroll() {
-    this.sections.forEach(section => {
-      const sectionTop = section.getBoundingClientRect().top
-      const isSectionVisible = (sectionTop - this.windowHalf) < 0
-  
-      if (isSectionVisible) {
-        section.classList.add('active')
-      } else if(section.classList.contains('active')) {
-        section.classList.remove('active')
+  getDistance() {
+    this.distance = [...this.sections].map(section => {
+      const offset = section.offsetTop
+
+      return {
+        element: section,
+        offset: Math.floor(offset - this.windowHalf)
       }
+    })
+  }
+
+  checkDistance() {
+    this.distance.forEach(item => {
+      if(window.pageYOffset > item.offset) {
+        item.element.classList.add('active')
+     } else if (item.element.classList.contains('active')) {
+       item.element.classList.remove('active')
+     }
     })
   }
   
   init() {
-    this.animationScroll()
-    window.addEventListener('scroll', this.animationScroll)
+    if(this.sections.length) {
+      this.getDistance()
+      this.checkDistance()
+      window.addEventListener('scroll', this.checkDistance)
+    }
     
     return this
+  }
+
+  stop() {
+    window.removeEventListener('scroll', this.checkDistance)
   }
 }
